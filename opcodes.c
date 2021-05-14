b83b42d636d1d5ae6921c9e3c67d5199b1613d41
jblanco75 <jblanco75@gmail.com>
2021-05-14 23:31:28 +0000

    Fix error

--- opcodes.c
+++ opcodes.c
@@ -93,7 +93,7 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't open an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		freestack(*stack);
 		exit(EXIT_FAILURE);
 	}
